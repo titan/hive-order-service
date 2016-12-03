b12d32b196f5ccca9fbcd85939d7f9362bdc0b10
Titan <howay.tan@gmail.com>
2016-12-03 15:37:54 +0800

    Refactor placeAnSaleOrder in processor

--- src/order-processor.ts
+++ src/order-processor.ts
@@ -231,7 +231,7 @@ processor.call("placeAnPlanOrder", (ctx: ProcessorContext, domain: string, uid: 
       const sum2 = sum + "";
       const sum1 = formatNum(sum2, 3);
       const order_no = "1" + "110" + "001" + sum1 + year + strno;
-      await db.query("BEGIN", []);
+      await db.query("BEGIN");
       await db.query("INSERT INTO orders(id, vid, type, state_code, state, summary, payment, no) VALUES($1,$2,$3,$4,$5,$6,$7,$8)", [order_id, vid, type, state_code, state, summary, payment, order_no]);
       for (const pid of Object.keys(plans)) {
         await db.query("INSERT INTO plan_order_ext(oid, pmid, promotion, pid, qid, service_ratio, expect_at, vehicle_real_value) VALUES($1,$2,$3,$4,$5,$6,$7,$8)", [order_id, pmid, promotion, pid, qid, service_ratio, expect_at, v_value]);
@@ -240,7 +240,7 @@ processor.call("placeAnPlanOrder", (ctx: ProcessorContext, domain: string, uid: 
           await db.query("INSERT INTO order_items(id, oid, piid, price) VALUES($1,$2,$3,$4)", [item_id, order_id, piid, plans[pid][piid]]);
         }
       }
-      await db.query("COMMIT", []);
+      await db.query("COMMIT");
       await cache.setexAsync(cbflag, 30, JSON.stringify({
         code: 200,
         data: { id: order_id, no: order_no }
@@ -249,7 +249,7 @@ processor.call("placeAnPlanOrder", (ctx: ProcessorContext, domain: string, uid: 
     } catch (e) {
       log.error(e);
       try {
-        await db.query("ROLLBACK", []);
+        await db.query("ROLLBACK");
       } catch (e1) {
         log.error(e1);
       }
@@ -423,13 +423,14 @@ processor.call("placeAnDriverOrder", (ctx: ProcessorContext, domain: string, uid
   const type = 1;
   (async () => {
     try {
-      await db.query("BEGIN", []);
+      await db.query("BEGIN");
       await db.query("INSERT INTO order_events(id, oid, uid, data) VALUES($1,$2,$3,$4)", [event_id, order_id, uid, "添加驾驶人"]);
       await db.query("INSERT INTO orders(id, vid, type, state_code, state, summary, payment) VALUES($1,$2,$3,$4,$5,$6,$7)", [order_id, vid, type, state_code, state, summary, payment]);
 
       for (const did of dids) {
         await db.query("INSERT INTO driver_order_ext(oid,pid) VALUES($1,$2)", [order_id, did]);
       }
+      await db.query("COMMIT");
       await sync_driver_orders(db, cache, domain, order_id);
       await cache.setexAsync(cbflag, 30, JSON.stringify({
         code: 200,
@@ -509,6 +510,176 @@ processor.call("updateOrderState", (ctx: ProcessorContext, domain: string, uid: 
       }), (err, result) => {
         done();
       });
+    }
+  })();
+});
+
+async function sync_sale_orders(db: PGClient, cache: RedisClient, domain: string, uid: string, oid?: string): Promise<void> {
+  const result = await db.query("SELECT o.id AS o_id, o.no AS o_no, o.vid AS o_vid, o.type AS o_type, o.state_code AS o_state_code, o.state AS o_state, o.summary AS o_summary, o.payment AS o_payment, o.start_at AS o_start_at, o.stop_at AS o_stop_at, o.created_at AS o_created_at, o.updated_at AS o_updated_at, e.qid AS e_qid, e.opr_level AS e_opr_level, oi.id AS oi_id, oi.pid AS oi_pid, oi.price AS oi_price, oi.piid AS oi_piid  FROM sale_order_ext AS e INNER JOIN orders AS o ON o.id = e.oid INNER JOIN plans AS p ON e.pid = p.id INNER JOIN plan_items AS pi ON p.id = pi.pid INNER JOIN order_items AS oi ON oi.piid = pi.id AND oi.oid = o.id WHERE o.deleted = FALSE AND e.deleted = FALSE AND p.deleted = FALSE AND pi.deleted = FALSE AND oi.deleted = FALSE" + (oid ? " AND o.id = $1" : ""), oid ? [oid] : []);
+  const orders = {};
+  for (const row of result.rows) {
+    if (orders.hasOwnProperty(row.o_id)) {
+      orders[row.o_id]["items"].push({
+        id: row.oi_id,
+        oid: row.o_id,
+        piid: row.oi_piid,
+        plan_item: null,
+        price: row.oi_price
+      });
+    } else {
+      const order = {
+        id: row.o_id,
+        no: trim(row.o_no),
+        type: row.o_type,
+        state_code: row.o_state_code,
+        state: trim(row.o_state),
+        summary: row.o_summary,
+        payment: row.o_payment,
+        start_at: row.o_start_at,
+        stop_at: row.o_stop_at,
+        vid: row.o_vid,
+        vehicle: null,
+        pid: row.e_pid,
+        opr_level: row.e_opr_level,
+        plan: null,
+        qid: row.e_qid,
+        quotation: null,
+        items: [{
+          id: row.oi_id,
+          oid: row.o_id,
+          piid: row.oi_piid,
+          plan_item: null,
+          price: row.oi_price
+        }],
+        created_at: row.o_created_at,
+        updated_at: row.o_updated_at
+      };
+      orders[row.o_id] = order;
+    }
+  }
+
+  const oids = Object.keys(orders);
+  const vidstmp = [];
+  const qidstmp = [];
+  const pidstmp = [];
+  const piidstmp = [];
+  for (const oid of oids) {
+    vidstmp.push(orders[oid]["vid"]);
+    qidstmp.push(orders[oid]["qid"]);
+    pidstmp.push(orders[oid]["pid"]);
+    for (const item of orders[oid]["items"]) {
+      piidstmp.push(item["plan_item"]);
+    }
+  }
+  const vids = [... new Set(vidstmp)];
+  const qids = [... new Set(qidstmp)];
+  const pids = [... new Set(pidstmp)];
+
+  const pvs = vids.map(vid => rpc<Object>(domain, process.env["VEHICLE"], null, "getVehicle", vid)); // fetch vehicle in parallel
+  const vreps = await Promise.all(pvs);
+  const vehicles = vreps.filter(v => v["code"] === 200).map(v => v["data"]);
+  for (const vehicle of vehicles) {
+    for (const oid of oids) {
+      const order = orders[oid];
+      if (vehicle["id"] === order["vid"]) {
+        order["vehicle"] = vehicle; // a vehicle may belong to many orders
+      }
+    }
+  }
+  const pqs = qids.map(qid => rpc<Object>(domain, process.env["QUOTATION"], null, "getQuotation", qid)); // fetch quotation in parallel
+  const qreps = await Promise.all(pqs);
+  const quotations = qreps.filter(q => q["code"] === 200).map(q => q["data"]);
+  for (const quotation of quotations) {
+    for (const oid of oids) {
+      const order = orders[oid];
+      if (quotation["id"] === order["qid"]) {
+        order["quotation"] = quotation;
+        break; // a quotation only belongs to an order
+      }
+    }
+  }
+  const pps = pids.map(pid => rpc<Object>(domain, process.env["plan"], null, "getPlan", pid)); // fetch plan in parallel
+  const preps = await Promise.all(pps);
+  const plans = preps.filter(p => p["code"] === 200).map(p => p["data"]);
+  for (const oid of oids) {
+    const order = orders[oid];
+    for (const plan of plans) {
+      if (plan["id"] === order["pid"]) {
+        order["plan"] = plan; // a plan may belong to many orders
+      }
+      for (const planitem of plan["items"]) {
+        for (const orderitem of order["items"]) {
+          if (planitem["id"] === orderitem["piid"]) {
+            orderitem["plan_item"] = planitem;
+          }
+        }
+      }
+    }
+  }
+  const multi = cache.multi();
+  for (const oid of oids) {
+    const order = orders[oid];
+    const vid = order["vehicle"]["vid"];
+    const updated_at = order.updated_at.getTime();
+    multi.zadd("orders", updated_at, oid);
+    multi.hset("vid-soid", vid, oid);
+    multi.hset("orderid-vid", oid, vid);
+    multi.hset("order-entities", oid, JSON.stringify(order));
+    multi.zadd("sale-orders", updated_at, oid);
+  }
+  return multi.execAsync();
+}
+
+processor.call("placeAnSaleOrder", (ctx: ProcessorContext, uid: string, domain: any, order_id: string, vid: string, pid: string, qid: string, items: Object[], summary: number, payment: number, opr_level: number, cbflag: string) => {
+  log.info(`placeAnSaleOrder, uid: ${uid}, domain: ${domain}, order_id: ${order_id}, vid: ${vid}, pid: ${pid}, qid: ${qid}, items: ${items}, summary: ${summary}, payment: ${payment}, opr_level: ${opr_level}, cbflag: ${cbflag}`);
+  const db: PGClient = ctx.db;
+  const cache: RedisClient = ctx.cache;
+  const done = ctx.done;
+  const item_id = uuid.v1();
+  const event_id = uuid.v1();
+  const state_code = 1;
+  const state = "已创建订单";
+  const type = 2;
+  const sale_id = uuid.v1();
+  const sale_data = JSON.stringify("新增第三方代售订单");
+  const piids = [];
+  for (const item in items) {
+    piids.push(item);
+  }
+  (async () => {
+    try {
+      await db.query("BEGIN");
+      await db.query("INSERT INTO orders(id, vid, type, state_code, state, summary, payment) VALUES($1, $2, $3, $4, $5, $6, $7)", [order_id, vid, type, state_code, state, summary, payment]);
+      await db.query("INSERT INTO order_events(id, oid, uid, data) VALUES($1, $2, $3, $4)", [event_id, order_id, uid, sale_data]);
+      await db.query("INSERT INTO sale_order_ext(oid, pid, qid, opr_level) VALUES($1, $2, $3, $4)", [order_id, pid, qid, opr_level]);
+      for (const piid of piids) {
+        const item_id = uuid.v1();
+        await db.query("INSERT INTO order_items(id, piid, oid, price) VALUES($1, $2, $3, $4)", [item_id, piid, order_id, items[piid]]);
+      }
+      await db.query("COMMIT");
+    } catch (err) {
+      log.error(err);
+      try {
+        await db.query("ROLLBACK");
+      } catch (err1) {
+        log.error(err1);
+      }
+      cache.setex(cbflag, 30, JSON.stringify({
+        code: 500,
+        msg: err.message
+      }), (err, result) => {
+        done();
+      });
+    }
+    try {
+      await cache.setexAsync(cbflag, 30, JSON.stringify({
+        code: 200,
+        data: order_id
+      }));
+      await sync_sale_orders(db, cache, domain, order_id);
+      done();
+    } catch (e) {
+      log.error(e);
     }
   })();
 });
